fe0d18e8121a92134fc0e5be38672e8808863ba8
Neil Smith <neil.git@njae.me.uk>
2016-12-03 20:43:35 +0000

    Day 3

---
+++ advent03.hs
@@ -0,0 +1,29 @@
+import Data.List
+import Data.List.Split
+
+type Triple = [Integer]
+
+main :: IO ()
+main = do 
+        instrText <- readFile "advent03.txt" 
+        let triangles = map (parseLine) $ lines instrText
+        part1 triangles
+        part2 triangles
+
+
+part1 :: [Triple] -> IO ()
+part1 triangles = do 
+    print $ length $ filter (validTriangle) triangles 
+
+part2 :: [Triple] -> IO ()
+part2 triangles = do 
+    print $ length $ filter (validTriangle) $ byColumns triangles 
+
+
+parseLine :: String -> Triple
+parseLine = map (read) . filter (not . null) . splitOn " "
+
+validTriangle triple = sortedTriple!!0 + sortedTriple!!1 > sortedTriple!!2
+    where sortedTriple = sort triple
+
+byColumns triangles = chunksOf 3 $ concat $ transpose triangles
