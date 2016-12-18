9c8d506500086e8584387782bc8978d13ffced02
Neil Smith <neil.git@njae.me.uk>
2016-12-18 11:19:23 +0000

    Added version of day 18 that uses a fold. It's more complex and slower.

--- advent18f.hs
+++ advent18f.hs
@@ -10,11 +10,12 @@ main = do
         part2
 
 part1 :: IO ()
--- part1 = putStrLn $ unlines $ map (showRow) $ take 10 $ iterate nextRow $ readRow input
-part1 = print $ length $ filter (not) $ concat $ take 40 $ iterate nextRow $ readRow input
+part1 = print $ fst $ foldl nextRowFold (countSafe row, row) [2..40]
+    where row = readRow input
 
 part2 :: IO ()
-part2 = print $ length $ filter (not) $ concat $ take 400000 $ iterate nextRow $ readRow input
+part2 = print $ fst $ foldl nextRowFold (countSafe row, row) [2..400000]
+    where row = readRow input
 
 readRow :: String -> [Bool]
 readRow = map (=='^')
@@ -27,6 +28,13 @@ extended row = [False] ++ row ++ [False]
 
 nextRow :: [Bool] -> [Bool]
 nextRow = map (isTrap) . segments . extended
+
+nextRowFold :: (Int, [Bool]) -> Int -> (Int, [Bool])
+nextRowFold (n, row) _ = (n + countSafe newRow, newRow)
+    where newRow = nextRow row
+
+countSafe :: [Bool] -> Int
+countSafe = length . filter (not)
 
 segments :: [a] -> [[a]]
 segments = filter ((==3) . length) . map (take 3) . tails
